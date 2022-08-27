42906018cc3d7a54c7df5c4beba080c6a00b0089
Jacob Schnitker <58757966+middspace@users.noreply.github.com>
2022-08-27 14:28:30 -0400

    add the heyNow method

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -1,8 +1,15 @@
 package ver1;
 
 public class HelloWorld {
+	
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
+
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello From Github");
 
 	}
